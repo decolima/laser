de17796064e3f714d4acc7aeccc8d3a03a6eefd1
Argjent97 <argjentkrasniqi97@gmail.com>
2022-05-23 10:38:14 +0200

    update frontend

--- bookmark/bkmapp-fr/js/login.js
+++ bookmark/bkmapp-fr/js/login.js
@@ -1,6 +1,107 @@
-console.log("test git");
-<<<<<<< HEAD
-console.log("prova porva")
-=======
-console.log("test");
->>>>>>> 2c9b05248aa8941561c9fe7db015551fed046ca1
+function init() {
+    let usr = sessionStorage.getItem("mail");
+    if (usr) // loggato
+    {
+        document.getElementById("loggeduser").innerHTML = usr;
+    }
+
+    else //non loggato
+    {
+        document.getElementById("loggeduser").innerHTML = " nessun utente";
+    }
+}
+function logout() {
+    sessionStorage.removeItem("globaljwt");
+    sessionStorage.clear();
+    let jwt = sessionStorage.getItem("globaljwt");
+    window.location.href = "login.html";
+
+}
+
+function login() {
+    let url = "http://192.168.0.125:8080/bkmapp/resources/users/login";
+    let usr = document.querySelector("#lusr").value;
+    let pwd = document.querySelector("#lpwd").value;
+    let postdata = {
+        "usr": usr,
+        "pwd": pwd
+    };
+
+    postdata = JSON.stringify(postdata);
+    fetch(url,
+        {
+            method: "post",
+            body: postdata,
+            headers: {
+                "Accept": 'application/json',
+                "Content-type": 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.status == 401) {
+                //alert("username errata");
+            }
+            else
+                return response.json();
+        })
+        .then(jsobj => {
+            if (jsobj != undefined || true) {
+                sessionStorage.setItem("globaljwt", jsobj.token);
+                sessionStorage.setItem("mail", jsobj.user);
+                sessionStorage.setItem("id", jsobj.userid);
+                sessionStorage.setItem("first_name", jsobj.fname);
+                sessionStorage.setItem("last_name", jsobj.lname);
+                document.querySelector("#loggeduser").innerHTML = sessionStorage.getItem("mail");
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            document.querySelector("#loggeduser").innerHTML = " nessun utente";
+
+        }
+
+        );
+
+
+
+}
+
+function registration() {
+    let url = "http://192.168.0.125:8080/bkmapp/resources/users";
+    let usr = document.querySelector("#rusr").value;
+    let pwd = document.querySelector("#rpwd").value;
+    let fname = document.querySelector("#rfirst_name").value;
+    let lname = document.querySelector("#rlast_name").value;
+    let postdata = {
+        "email": usr,
+        "pwd": pwd,
+        "first_name": fname,
+        "last_name": lname
+    };
+    postdata = JSON.stringify(postdata);
+    fetch(url,
+        {
+            method: "post",
+            body: postdata,
+            headers: {
+                "Accept": 'application/json',
+                "Content-type": 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.status == 401) {
+                //alert("username errata");
+            }
+            else
+                return response.json();
+        })
+        .catch(error => {
+            console.log(error);
+        }
+
+        );
+
+
+
+}
+
