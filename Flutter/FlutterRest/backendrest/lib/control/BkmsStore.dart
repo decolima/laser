2d3cb5eb0c57a7c7bfaf4d9aa25a0773033de92f
Andre Lima <andrelima@macbookpro-655d.laser.lan>
2022-06-08 15:01:22 +0200

    backend flutter

--- Flutter/FlutterRest/backendrest/lib/control/BkmsStore.dart
+++ Flutter/FlutterRest/backendrest/lib/control/BkmsStore.dart
@@ -32,6 +32,7 @@ class bkmsStore {
           tags: "",
           utente: "",
           utenteagg: "",
+          mail: "",
           idbkm: 0 as Long);
     }
   }
@@ -74,6 +75,7 @@ class bkmsStore {
           tags: "",
           utente: "",
           utenteagg: "",
+          mail: "",
           idbkm: 0 as Long);
     }
   }
@@ -118,6 +120,7 @@ class bkmsStore {
           tags: "",
           utente: "",
           utenteagg: "",
+          mail: "",
           idbkm: 0 as Long);
     }
   }
@@ -150,65 +153,8 @@ class bkmsStore {
           tags: "",
           utente: "",
           utenteagg: "",
+          mail: "",
           idbkm: 0 as Long);
     }
   }
 }
-
-
-
-
-
-
-/*
-import 'package:flutter/material.dart';
-import '../entity/bkms.dart';
-import '../service/rest.dart';
-
-class BkmsStore extends StatefulWidget {
-  @override
-  State<BkmsStore> createState() => _BkmsStore();
-}
-
-class _BkmsStore extends State<BkmsStore> {
-  final _bkms = <Bkms>[];
-
-  _getBkms() {
-    try {
-      rest.getRest("/bkms", false, "").then((response) {
-        setState(() {
-          for (var e in response) {
-            _bkms.add(Bkms.fromJson(e));
-          }
-        });
-      });
-    } catch (e) {
-      print("Problema per caricare Bkms ...");
-    }
-  }
-
-  _Bkms() {
-    _getBkms();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Lista Bkms"),
-      ),
-      body: getBkms(),
-    );
-  }
-
-  getBkms() {
-    return ListView.builder(
-        itemCount: _bkms.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-              title: Text(_bkms[index].descrizione.toString()),
-              subtitle: Text(_bkms[index].idbkm.toString()));
-        });
-  }
-}
-*/
